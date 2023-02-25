8e4bfa467712e51687a5b227eadd6a3822baf6e7
liu_zhao_feng_PC <liu_zhao_feng_alex@163.com>
2023-02-25 13:53:43 +0800

    增加了微信扫码登录

--- src/router/index.js
+++ src/router/index.js
@@ -1,9 +1,15 @@
 import {createRouter, createWebHashHistory} from 'vue-router'
+import index from '../views/IndexView.vue'
 import login from '../views/LoginView.vue'
 
 const router = createRouter({
         history: createWebHashHistory(),
         routes: [
+            {
+                path: "/",
+                name: "index",
+                component: index
+            },
             {
                 path: "/login",
                 name: "login",
