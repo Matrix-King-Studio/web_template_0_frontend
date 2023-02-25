3e7425061c98194499fa3fe41c4e31a27f54877b
liu_zhao_feng_PC <liu_zhao_feng_alex@163.com>
2023-02-25 14:19:15 +0800

    增加了微信扫码登录

--- src/api/user.js
+++ src/api/user.js
@@ -3,7 +3,7 @@ import service from "@/api/base";
 export default {
     wx_login(code) {
         return service({
-            url: `/wx_login/?code=${code}`,
+            url: `/accounts/weixin/login/callback/?code=${code}`,
             method: "get",
         })
     },
--- src/views/LoginView.vue
+++ src/views/LoginView.vue
@@ -80,6 +80,7 @@
 </template>
 <script>
 import {reactive} from 'vue'
+import user_api from '@/api/user'
 
 export default {
   name: 'LoginView',
@@ -113,17 +114,30 @@ export default {
   },
   methods: {
     get_wx_qrcode() {
-      let wx_login = new WxLogin({
+      new WxLogin({
         id: "wx_login_container",
         appid: "wx53cf447461989356",
         scope: "snsapi_login",
         redirect_uri: encodeURIComponent("http://www.template.matrix-studio.top/#/login"),
-        state: Math.ceil(Math.random() * 100),
         style: "black",
         href: "",
         self_redirect: false,
       });
-    }
+      this.check_wx_qrcode();
+    },
+    check_wx_qrcode() {
+      this.checkQRCodeInterval = setInterval(() => {
+        console.log(this.$route.query);
+        if (this.$route.query.code) {
+          clearInterval(this.checkQRCodeInterval);
+          user_api.wx_login(this.$route.query.code).then(res => {
+            console.log(res);
+          }).catch(err => {
+            console.log(err);
+          })
+        }
+      }, 1000);
+    },
   }
 }
 </script>
